858ae662d39f12c35f3b458f7cf050ae81f7557e
nathantokarz <nathantokarz@gmail.com>
2022-03-31 13:27:03 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,38 @@
 import Routes from "./routes";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { AppBar, Box, Toolbar, Container, Typography } from "@mui/material";
-import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+import { Link, AppBar, Avatar, Box, Toolbar, Container } from "@mui/material";
 const theme = createTheme();
+
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-       <AppBar position = "relative">
-        <Toolbar>
-          <ImageSearchIcon sx={{mr:1}} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Rarity Tool
-          </Typography>
-        </Toolbar>
-      </AppBar> 
-      <main>
-        <Box>
-          <Container maxWidth="md">
-            <Routes />
-          </Container>
-        </Box>
-      </main>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <AppBar style={{ background: '#FFFFFF', opacity: "0.8" }} position="relative">
+                <Box sx={{ display: "flex", padding: "0px 20px 0px 20px", justifyContent: "space-between" }}>
+                    <Avatar
+                        alt="Avatar"
+                        src="/img/logo.png"
+                        position="flex"
+                        sx={{ width: 250, height: 130, padding: "0px 0px 0px 10px" }}
+                    ></Avatar>
+                    <Toolbar position="right">
+                        <Link href="https://warsomewizards.com/" underline="hover" color="black" padding="0 25px"><b>Home</b></Link>
+                        <Link href="https://warsomewizards.com/team/" underline="hover" color="black" padding="0 25px"><b>Team</b></Link>
+                        <Link href="https://warsomewizards.com/gallery/" underline="hover" color="black" padding="0 25px"><b>Gallery</b></Link>
+                        <Link href="https://warsomewizards.com/faqs/" underline="hover" color="black" padding="0 25px"><b>FAQ</b></Link>
+                        <Link href="https://warsomewizards.com/news/" underline="hover" color="black" padding="0 25px"><b>News</b></Link>
+                        <Link href="https://discord.com/invite/2YFTnWGHQH" underline="hover" color="black" padding="0 25px"><b>Join</b></Link>
+                    </Toolbar>
+                </Box>
+            </AppBar>
+            <main>
+                <Box>
+                    <Container maxWidth="md">
+                        <Routes />
+                    </Container>
+                </Box>
+            </main>
+        </ThemeProvider>
+    );
 }
 
 export default App;
