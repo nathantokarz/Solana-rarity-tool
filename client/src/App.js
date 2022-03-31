ba622cf3efe1fb78c0130de5da6f0a5752dabab1
nathantokarz <nathantokarz@gmail.com>
2022-03-31 14:13:48 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,18 @@ const theme = createTheme();
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <AppBar style={{ background: 'black', opacity: "0.8" }} position="relative">
+            <AppBar style={{ background: 'black', opacity: "0.7" }} position="relative">
                 <Box sx={{ display: "flex", padding: "0px 20px 0px 20px", justifyContent: "space-between" }}>
                     <Avatar
                         alt="Avatar"
                         src="/img/logo.png"
                         position="flex"
-                        sx={{ width: 250, height: 130, padding: "0px 0px 0px 10px" }}
+                        sx={{ width: 150, height: 100, padding: "0px 0px 0px 10px" }}
                     ></Avatar>
                     <Toolbar position="right">
                         <Link href="https://warsomewizards.com/" underline="hover" color="white" padding="0 25px"><b>Home</b></Link>
                         <Link href="https://warsomewizards.com/team/" underline="hover" color="white" padding="0 25px"><b>Zuse</b></Link>
-                        <Link href="https://warsomewizards.com/gallery/" underline="hover" color="white" padding="0 25px"><b>Ug</b></Link>
+                        <Link href="https://warsomewizards.com/gallery/" underline="hover" color="white" padding="0 25px"><b>UGA-Mart</b></Link>
                     </Toolbar>
                 </Box>
             </AppBar>
