2a4a5dfba95be951bdd7f5009a77f02ba78ff13f
nathantokarz <nathantokarz@gmail.com>
2022-03-31 19:13:47 -0600

    Update index.js

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -119,13 +119,13 @@ export function HomeView() {
   return (
     <Paper sx={{ mt: 10 }}>
       <Grid container spacing={0}>
-        <Grid item md={6} xs={12} style={{ backgroundColor: "transparent" }}>
+        <Grid item md={6} xs={12} style={{ background: "none" }}>
           {Header}
         </Grid>
         <Grid
           container
           item
-          sx={{ display: "flex", backgroundCcolor: "transparent" }}
+          sx={{ display: "flex", background: "none" }}
           md={6}
           xs={12}
         >
